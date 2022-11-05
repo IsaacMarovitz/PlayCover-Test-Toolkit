8c63a8740211ae738f327c59fdd2201fa7317d51
Isaac Marovitz <isaacryu@icloud.com>
2022-11-05 00:19:31 -0400

    Init

---
+++ PCTT/Assets/Scripts/SystemInfoCheck.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+public class SystemInfoCheck : MonoBehaviour {
+
+    public TMP_Text deviceModel;
+    public TMP_Text deviceName;
+    public TMP_Text deviceType;
+    public TMP_Text deviceUniqueIdentifier;
+    public TMP_Text graphicsDeviceName;
+    public TMP_Text graphicsDeviceID;
+    public TMP_Text graphicsDeviceType;
+    public TMP_Text graphicsDeviceVendor;
+    public TMP_Text graphicsDeviceVendorID;
+    public TMP_Text graphicsDeviceVersion;
+    public TMP_Text graphicsMemorySize;
+    public TMP_Text graphicsShaderLevel;
+    public TMP_Text operatingSystem;
+    public TMP_Text operatingSystemFamily;
+    public TMP_Text processorCount;
+
+    void Start() {
+        deviceModel.text = "Device model: " + SystemInfo.deviceModel;
+        deviceName.text = "Device name: " + SystemInfo.deviceName;
+        deviceType.text = "Device type: " + SystemInfo.deviceType.ToString();
+        deviceUniqueIdentifier.text = "Device unique identifier: " + SystemInfo.deviceUniqueIdentifier;
+        graphicsDeviceName.text = "Graphics device name: " + SystemInfo.graphicsDeviceName;
+        graphicsDeviceID.text = "Graphics device id: " + SystemInfo.graphicsDeviceID.ToString();
+        graphicsDeviceType.text = "Graphics device type: " + SystemInfo.graphicsDeviceType.ToString();
+        graphicsDeviceVendor.text = "Graphics device vendor: " + SystemInfo.graphicsDeviceVendor;
+        graphicsDeviceVendorID.text = "Graphics device vendor id: " + SystemInfo.graphicsDeviceVendorID.ToString();
+        graphicsDeviceVersion.text = "Graphics device version: " + SystemInfo.graphicsDeviceVersion;
+        graphicsMemorySize.text = "Graphics memory size: " + SystemInfo.graphicsMemorySize.ToString();
+        graphicsShaderLevel.text = "Graphics shader level: " + SystemInfo.graphicsShaderLevel.ToString();
+        operatingSystem.text = "Operating system: " + SystemInfo.operatingSystem;
+        operatingSystemFamily.text = "Operating system family: " + SystemInfo.operatingSystemFamily.ToString();
+        processorCount.text = "Processor count: " + SystemInfo.processorCount.ToString();
+    }
+}
